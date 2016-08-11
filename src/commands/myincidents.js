4d6399c67db414b7ae756bbdbd1b804cd0931238
dwjanus <devin.janus@samanage.com>
2016-08-11 17:50:35 -0400

    trying to make an incident slash command

--- src/commands/myincidents.js
+++ src/commands/myincidents.js
@@ -13,7 +13,7 @@ var attachments = {
     title: 'Incidents',
     color: '#0067B3',
     text: 'This will contain a list with your 5 most recent incidents',
-    mrkdown_in: ['text']
+    mrkdown_in: ['text', 'pretext']
   };
 
 const handler = (payload, res) => {
