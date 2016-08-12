354684d37e4c71097cdf43c2c5a0ab3ea837d583
dwjanus <devin.janus@samanage.com>
2016-08-12 16:32:13 -0400

    compiled locally so i guess lets see

--- src/samanage.js
+++ src/samanage.js
@@ -1,9 +1,9 @@
 
 const _ = require('lodash');
-const config = require('../config');
+const config = require('./config');
 const request = require('request');
+const api_url = 'https://api.samanage.com/';
 
-var api_url: 'https://api.samanage.com/';
 // authorize the api user
 request.get(api_url).auth('devin.janus@samanage.com', 'BenHobgood666', false);
 
@@ -13,15 +13,15 @@ var client = new RequestClient(api_url);
 
 var incident_list;
 
-function my_incidents (email, callback) {
-  var samanage_id = JSON.parse(client.get({'uri': 'users.json', 'query': {'email': email}}));
-  incident_list = JSON.parse(client.get({'uri': 'incidents.json', 'query': {'assigned_to': + samanage_id}}));
-  callback(null, incident_list);
+module.exports = {
+  my_incidents: function (email, callback) {
+    var samanage_id = JSON.parse(client.get({'uri': 'users.json', 'query': {'email': email}}));
+    incident_list = JSON.parse(client.get({'uri': 'incidents.json', 'query': {'assigned_to': + samanage_id}}));
+    callback(null, incident_list);
+  },
+    
+  new_incidents: function (callback) {
+    incident_list = JSON.parse(client.get({'uri': 'incidents.json'}));
+    callback(null, incident_list);
+  }
 };
-  
-function new_incidents (callback) {
-  incident_list = JSON.parse(client.get({'uri': 'incidents.json'}));
-  callback(null, incident_list);
-};
-
-module.exports = incidents;
