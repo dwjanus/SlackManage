25ab41ec7222e5d5b19398f573db2f6354621e3e
dwjanus <devin.janus@samanage.com>
2016-08-15 15:16:05 -0400

    trying to test incidents

--- src/lib/samanage.js
+++ src/lib/samanage.js
@@ -15,8 +15,6 @@ const options = {
 
 module.exports = {
 
-  //var incident_list;
-
   my_incidents: function (email, callback) {
     var req = https.request(options, function (res) {
       console.log('STATUS: ' + res.statusCode);
@@ -25,6 +23,10 @@ module.exports = {
 
       res.on('data', function (chunk) {
         console.log('BODY: ' + chunk);
+
+        var samanage_id = JSON.parse(client.get({'uri': 'users.json', 'query': {'email': email}}));
+        var incident_list = JSON.parse(client.get({'uri': 'incidents.json', 'query': {'assigned_to': + samanage_id}}));
+        callback(null, incident_list);
       });
 
       req.on('error', function(e) {
@@ -32,11 +34,7 @@ module.exports = {
       });
 
       req.end();
-    });
-
-    // var samanage_id = JSON.parse(client.get({'uri': 'users.json', 'query': {'email': email}}));
-    // incident_list = JSON.parse(client.get({'uri': 'incidents.json', 'query': {'assigned_to': + samanage_id}}));
-    // callback(null, incident_list);
+    }); 
   },
     
   new_incidents: function (callback) {
@@ -47,6 +45,8 @@ module.exports = {
 
       res.on('data', function (chunk) {
         console.log('BODY: ' + chunk);
+        var incident_list = JSON.parse(client.get({'uri': 'incidents.json'}));
+        callback(null, incident_list);
       });
 
       req.on('error', function(e) {
@@ -54,9 +54,6 @@ module.exports = {
       });
 
       req.end();
-    });
-
-    // incident_list = JSON.parse(client.get({'uri': 'incidents.json'}));
-    // callback(null, incident_list);
+    }); 
   }
 };
