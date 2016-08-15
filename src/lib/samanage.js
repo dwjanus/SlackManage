b5aeb4c82be08987a4b80f4316f0c42de7afe7cf
dwjanus <devin.janus@samanage.com>
2016-08-15 14:52:23 -0400

    trying to test incidents

--- src/lib/samanage.js
+++ src/lib/samanage.js
@@ -1,27 +1,59 @@
 
 const _ = require('lodash');
 const config = require('../config');
-const request = require('request');
-const RequestClient = require('reqclient').RequestClient;
-const api_url = 'https://api.samanage.com/';
-
-// authorize the api user
-request.get(api_url).auth('devin.janus@samanage.com', 'BenHobgood666', false);
-
-// this will handle our api requests
-var client = new RequestClient(api_url);
-
-var incident_list;
+var https = require('https');
 
 module.exports = {
+
+  var options = {
+    host: 'https://api.samanage.com/',
+    path: '/incidents.json',
+    method: 'GET',
+    auth: username:env.process.username + ':' + password:env.process.password
+  };
+
+  var incident_list;
+
   my_incidents: function (email, callback) {
-    var samanage_id = JSON.parse(client.get({'uri': 'users.json', 'query': {'email': email}}));
-    incident_list = JSON.parse(client.get({'uri': 'incidents.json', 'query': {'assigned_to': + samanage_id}}));
-    callback(null, incident_list);
+    var req = https.request(options, function (res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncodeing('utf8');
+
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+
+      req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+      });
+
+      req.end();
+    });
+
+    // var samanage_id = JSON.parse(client.get({'uri': 'users.json', 'query': {'email': email}}));
+    // incident_list = JSON.parse(client.get({'uri': 'incidents.json', 'query': {'assigned_to': + samanage_id}}));
+    // callback(null, incident_list);
   },
     
   new_incidents: function (callback) {
-    incident_list = JSON.parse(client.get({'uri': 'incidents.json'}));
-    callback(null, incident_list);
+    var req = https.request(options, function (res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncodeing('utf8');
+
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+
+      req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+      });
+
+      req.end();
+    });
+
+    // incident_list = JSON.parse(client.get({'uri': 'incidents.json'}));
+    // callback(null, incident_list);
   }
 };
