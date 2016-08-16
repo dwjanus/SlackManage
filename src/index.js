c8546de343dc94ebc2ffd9734850f844c4daf741
dwjanus <devin.janus@samanage.com>
2016-08-16 16:39:52 -0400

    slowly working through the json object rn

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ function samanage() {
     response.on('end', function () {
       var parsedResponse = JSON.parse(body);
       console.log('BODY: ' + JSON.stringify(parsedResponse) + "\n\n");
-      console.log('First Incident: '+ parsedResponse.incident[0]) + '\n';
+      console.log('First Incident: ' + parsedResponse.id[0]) + '\n';
     });
         // var incident = chunk.Incident[i];
         // console.log("INCIDENT " + i + ": " + incident + "\n");
