afe1ab9c57b8afa8f4455564b085d0b7e0ad81fc
dwjanus <devin.janus@samanage.com>
2016-08-17 17:09:03 -0400

    maybe almost works, had to fix variable in sendWebhook

--- src/bot.js
+++ src/bot.js
@@ -13,35 +13,35 @@ bot.started((payload) => {
 
 bot.message((msg) => {
   if (!msg.user) return;
-  if (!_.includes(msg.text.match(/<@[A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
+  //if (!_.includes(msg.text.match(/<@[A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
 
-  // id = msg.user;
-  // var options = {user: id};
-  // user = bot.api.users.info(options, function(err, res) {
-  //   var email = response.user.profile.email;
+  id = msg.user;
+  var options = {user: id};
+  user = bot.api.users.info(options, function(err, res) {
+    var email = response.user.profile.email;
 
-  //   slack.chat.postMessage({
-  //     token: config('SLACK_TOKEN'),
-  //     icon_emoji: config('ICON_EMOJI'),
-  //     channel: msg.channel,
-  //     username: "Samanage",
-  //     text: "You are: " + email
-  //   });
-  // });
-
-  slack.chat.postMessage({
-    token: config('SLACK_TOKEN'),
-    icon_emoji: config('ICON_EMOJI'),
-    channel: msg.channel,
-    username: 'Samanage',
-    text: `beep boop: What it do tho"`
-  }, (err, data) => {
-    if (err) throw err;
-
-    let txt = _.truncate(data.message.text);
-
-    console.log(`🤖  beep boop: I responded with "${txt}"`);
+    slack.chat.postMessage({
+      token: config('SLACK_TOKEN'),
+      icon_emoji: config('ICON_EMOJI'),
+      channel: msg.channel,
+      username: "Samanage",
+      text: "You are: " + email
+    });
   });
+
+  // slack.chat.postMessage({
+  //   token: config('SLACK_TOKEN'),
+  //   icon_emoji: config('ICON_EMOJI'),
+  //   channel: msg.channel,
+  //   username: 'Samanage',
+  //   text: `beep boop: What it do tho"`
+  // }, (err, data) => {
+  //   if (err) throw err;
+
+  //   let txt = _.truncate(data.message.text);
+
+  //   console.log(`🤖  beep boop: I responded with "${txt}"`);
+  // });
 });
 
 module.exports = bot;
