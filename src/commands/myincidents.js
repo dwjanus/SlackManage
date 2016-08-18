05bf8a73213dd675795b810a07f675aae3d5cebc
dwjanus <devin.janus@samanage.com>
2016-08-18 14:46:54 -0400

    trying to figure out how to parse the payload in slash command handler

--- src/commands/myincidents.js
+++ src/commands/myincidents.js
@@ -18,7 +18,7 @@ const handler = (payload, res) => {
   var userid = payload.user_id;
   console.log('\n' + JSON.stringify(userid) + '\n');
   // get the user profile here:
-  var user = slack.users.info(userid);
+  var user = slack.api.users.info(userid);
   console.log('\n' + JSON.stringify(user) + '\n');
   // get the user's email here:
   var email = user.profile.email;
