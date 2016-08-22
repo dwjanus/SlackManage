9de453815a942e56cc1b4292628aada2a8c3c3fb
dwjanus <devin.janus@samanage.com>
2016-08-22 16:46:42 -0400

    trying to make my incidents play nice

--- src/commands/myincidents.js
+++ src/commands/myincidents.js
@@ -87,34 +87,7 @@ const handler = (payload, res) => {
             console.log('IS USER: ' + parsed_group.is_user + ' ' + typeof parsed_group.is_user  + '\n');
             if (parsed_group.is_user) {
               group_id = ids[0].toString();
-              console.log('GROUP_ID FOUND: ' + group_id + '\n');
-
-              var incidents = Samanage.my_incidents(group_id, size);
-
-              attachments = incidents.slice(0, size).map((incident) => {
-                return {
-                  title: `${incident.title}\n`,
-                  title_link: `${incident.title_link}`,
-                  pretext: `Ticket: ${incident.number} - Requested by: ${incident.requester}\n`,
-                  color: `${incident.color}`,
-                  text: `${incident.description}\n\n`,
-                  fields: [
-                    {
-                      title: 'State',
-                      value: `${incident.state}`,
-                      short: true
-                    },
-                    {
-                      title: 'Priority',
-                      value: `${incident.priority}`,
-                      short: true
-                    }
-                  ],
-                  footer: 'due on: ',
-                  ts: `${incident.ts}`,
-                  mrkdown_in: ['text', 'pretext']
-                }
-              });  
+              console.log('GROUP_ID FOUND: ' + group_id + '\n');  
             }
           });
         });
@@ -131,6 +104,33 @@ const handler = (payload, res) => {
     request.on('error', function (e) {
       console.log('problem with request: ' + e.message);
     });
+  });
+
+  var incidents = Samanage.my_incidents(group_id, size);
+
+  attachments = incidents.slice(0, size).map((incident) => {
+    return {
+      title: `${incident.title}\n`,
+      title_link: `${incident.title_link}`,
+      pretext: `Ticket: ${incident.number} - Requested by: ${incident.requester}\n`,
+      color: `${incident.color}`,
+      text: `${incident.description}\n\n`,
+      fields: [
+        {
+          title: 'State',
+          value: `${incident.state}`,
+          short: true
+        },
+        {
+          title: 'Priority',
+          value: `${incident.priority}`,
+          short: true
+        }
+      ],
+      footer: 'due on: ',
+      ts: `${incident.ts}`,
+      mrkdown_in: ['text', 'pretext']
+    }
   });
 
   let msg = _.defaults({
