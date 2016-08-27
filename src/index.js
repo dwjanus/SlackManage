d225a1edd655ec20e21c0d7d19525b8c82fbe212
dwjanus <devin.janus@samanage.com>
2016-08-27 16:36:40 -0400

    almost works, still having timeout issues tho

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.post('/commands/samanage', (req, res) => {
     return payload.text.match(cmd.pattern) ? cmd : a
   }, helpCommand);
 
-  res.sendStatus(200);
+  //res.sendStatus(200);
   //if pattern == mine then invoke other function with throws a quick output to current res, 
   // then invoke cmd.handler within a post request to postOptions (request_url)
   app.post(post, (request, response) => {
