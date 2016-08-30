3e0a0db119246f02f8f850806f4389f46624654f
dwjanus <devin.janus@samanage.com>
2016-08-30 18:17:08 -0400

    basically works, timeout issue resolved, now trying to get images into message body

--- src/lib/samanage.js
+++ src/lib/samanage.js
@@ -139,6 +139,7 @@ function my_incidents (group_id, callback) {
             color = "#E3E4E6";
 
           var image_html = parsedResponse[i].description;
+          var image_url;
           if (image_url.indexOf('src') !== -1) {
             image_url = image_html.split('src=').split(/[\s\"]/);
           } 
