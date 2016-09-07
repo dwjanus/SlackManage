493134576bc2240349c37c2e00c65b83aa442efd
dwjanus <devin.janus@samanage.com>
2016-09-07 14:57:13 -0400

    Added functions to samanage.js to handle searching for incident by ticket #

--- src/lib/samanage.js
+++ src/lib/samanage.js
@@ -345,7 +345,7 @@ function incidentRequest (options, perpage, number, callback) {
       //   callback(null, parsed[count].number, parsed[count].id);
       //   count++;
       // }
-      return callback(null, pop(parsed).number, pop(parsed).id);
+      return callback(null, parsed[perpage-1].number, parsed[perpage-1].id);
     });
   });
   request.end();
