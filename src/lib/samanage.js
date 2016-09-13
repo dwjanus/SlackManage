fb596b97a47079cebe1bd9c7519f25fcae4d4ca5
dwjanus <devin.janus@samanage.com>
2016-09-13 02:00:39 -0400

    Clean up Checkpoint

--- src/lib/samanage.js
+++ src/lib/samanage.js
@@ -127,7 +127,7 @@ function my_incidents (group_id, callback) {
           if (image_html.indexOf('src') !== -1) {
             image_url = image_html.split('src="')[1];
             console.log('ELEMENT after first split: ' + util.inspect(image_url) + '\n');
-            image_url = self.split(/[\s\"]/)[0];
+            image_url = image_url.split(/[\s\"]/)[0];
             console.log('ELEMENT after second split: ' + util.inspect(image_url) + '\n');
           }
 
@@ -208,7 +208,7 @@ function new_incidents (callback) {
         if (image_html.indexOf('src') !== -1) {
           image_url = image_html.split('src="')[1];
           console.log('ELEMENT after first split: ' + util.inspect(image_url) + '\n');
-          image_url = self.split(/[\s\"]/)[0];
+          image_url = image_url.split(/[\s\"]/)[0];
         }
 
         var current = {
@@ -347,7 +347,7 @@ function incident (options, callback) {
       var image_url = "";
       if (image_html.indexOf('src') !== -1) {
         image_url = image_html.split('src="')[1];
-        image_url = self.split(/[\s\"]/)[0];
+        image_url = image_url.split(/[\s\"]/)[0];
       }
 
       var current = {
