46d40a44d2d231b90c0b5c404b33898e4915953a
dwjanus <devin.janus@samanage.com>
2016-09-14 16:17:28 -0400

    showing signs of life, commands going through again

--- src/bot.js
+++ src/bot.js
@@ -20,25 +20,26 @@ bot.message((msg) => {
   if (!msg.user) return;
   if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
 
-  var bot_token = "";
+  
   client.hgetall(team, function (err, obj) {
-    bot_token = obj['bot_access_token'];
+    var bot_token = obj['bot_access_token'];
     console.log('bot_token = ' + bot_token + '\n');
+    
+    slack.chat.postMessage({
+      token: bot_token,
+      icon_emoji: config('ICON_EMOJI'),
+      channel: msg.channel,
+      username: 'Samanage bot',
+      text: 'beep boop: What it do tho'
+    }, (err, data) => {
+      if (err) throw err;
+
+      let txt = _.truncate(data.message.text);
+
+      console.log(`🤖  beep boop: I responded with "${txt}"`);
+    });
   });
 
-  slack.chat.postMessage({
-    token: bot_token,
-    icon_emoji: config('ICON_EMOJI'),
-    channel: msg.channel,
-    username: 'Samanage bot',
-    text: 'beep boop: What it do tho'
-  }, (err, data) => {
-    if (err) throw err;
-
-    let txt = _.truncate(data.message.text);
-
-    console.log(`🤖  beep boop: I responded with "${txt}"`);
-  });
 });
 
 module.exports = bot;
--- src/index.js
+++ src/index.js
@@ -35,12 +35,10 @@ app.get('/', (req, res) => {
 
 app.get('/auth', (req, res) => {
   var url = req.url;
-  console.log('url: ' + url + '\n');
   var codePos = url.indexOf("code="); //index where code starts in the url
   var codeStart = codePos + 5; //we dont want the 'code=' part
   var codeEnd = url.indexOf("&"); //we dont need anything else
   var accessCode = url.substring(codeStart, codeEnd).toString(); //put it all together
-  console.log('Access Code: ' + accessCode + '\n');
 
   var request = require('request');
   request('https://slack.com/api/oauth.access?client_id=' + process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET + '&code=' + accessCode,
@@ -64,71 +62,22 @@ app.get('/auth', (req, res) => {
           "bot_access_token": botAccessToken,
           "bot_user_id": botUserId
         });
-        
-        // client.hgetall(teamId.toString(), function (err, obj) {
-        //   if (err) console.log(err);
-        //   console.dir(obj);
-        // });
       }
   });
   res.sendStatus(200);
-
-  // // Verify user accepted auth request
-  // if (codePos > -1) {
-  //   var request = https.request({
-  //     host: 'slack.com',
-  //     path: '/api/oauth.access?client_id=' + process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET + '&code=' + accessCode,
-  //     method: 'POST',
-  //     headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
-  //   }, (response) => {
-  //     response.setEncoding('utf8');
-      
-  //     var body = "";
-  //     response.on('data', (chunk) => {
-  //       body += chunk;
-  //     });
-
-  //     console.log('Body: ' + util.inspect(body) + '\n');
-
-  //     response.on('end', () => {
-  //       var teamInfo = JSON.parse(body);
-  //       console.log('TeamInfo: ' + util.inspect(teamInfo) + '\n');
-  //       if (teamInfo.ok === true) {
-  //         // save the ACCESS_CODE
-  //         // client.set("ACCESS_TOKEN", teamInfo.access_token);
-  //         // client.set("WEBHOOK_URL", teamInfo.incoming_webhook.url);
-  //         // client.set("SLACK_TOKEN", teamInfo.bot.bot_access_token);
-  //         console.log('teamInfo is okay' + '\n');
-  //       } else {
-  //         // Error
-  //       }
-  //     });
-  //   });
-  //   request.end();
-
-  //   request.on('error', (e) => {
-  //     return callback(new Error("Problem with request: " + e.message));
-  //   });
-  // } else {
-  //   // Reroute user back to install page, they denied auth
-  // }
   return;
 });
 
 
 app.post('/commands/samanage', (req, res) => {
   let payload = req.body;
-  console.log('Command Payload: ' + util.inspect(payload) + '\n');
-  
   var team_id = payload.team_id;
 
   client.hgetall(team_id, function (err, obj) {
     if (err) console.log(err);
 
     var access = "";
-    console.log(util.inspect(obj) +'\n');
     access = obj["access_token"];
-    console.log('Access: ' + access + '\n');
 
     if (!payload || payload.token !== config('OAUTH_TOKEN')) {
       let err = '✋  Dowhatnow? An invalid slash token was provided\n' +
