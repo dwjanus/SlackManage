63726197adcea4d2e4b33cb41da48baaf03a48a5
dwjanus <devin.janus@samanage.com>
2016-09-15 00:05:55 -0400

    showing signs of life: attempting bot kit messaging and slack oauth.access

--- src/bot.js
+++ src/bot.js
@@ -6,11 +6,6 @@ const config = require('./config');
 const util = require('util');
 const slack = require('slack');
 var client = require('redis').createClient(process.env.REDIS_URL);
-const Botkit = require('botkit');
-
-var controller = Botkit.slackbot({
-  debug: false
-}).spawn();
 
 let bot = slack.rtm.client();
 var team;
@@ -21,11 +16,15 @@ bot.started((payload) => {
   team = payload.team.id;
 });
 
+bot.hello((message) => {
+  console.log(`Got a message: ${message}`);
+  bot.close();
+})
+
 bot.message((msg) => {
   if (!msg.user) return;
   if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
 
-  
   client.hgetall(team, function (err, obj) {
     var bot_token = obj['bot_access_token'];
     console.log('bot_token = ' + bot_token + '\n');
@@ -46,31 +45,6 @@ bot.message((msg) => {
   });
 });
 
-controller.hears('hello', 'direct_message, direct_mention', function (bot, msg) {  
-  bot.message((msg) => {
-    if (!msg.user) return;
-    if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
 
-    
-    client.hgetall(team, function (err, obj) {
-      var bot_token = obj['bot_access_token'];
-      console.log('bot_token = ' + bot_token + '\n');
-      
-      slack.chat.postMessage({
-        token: bot_token,
-        icon_emoji: config('ICON_EMOJI'),
-        channel: msg.channel,
-        username: 'Samanage bot',
-        text: 'beep boop: What it do tho'
-      }, (err, data) => {
-        if (err) throw err;
-
-        let txt = _.truncate(data.message.text);
-
-        console.log(`🤖  beep boop: I responded with "${txt}"`);
-      });
-    });
-  });
-});
 
 module.exports = bot;
